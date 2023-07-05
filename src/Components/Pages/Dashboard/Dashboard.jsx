ddb744e6c26e0782bf556eb7c1b92a40dac41fc6
manojbelbase <manojbelbase56@gmail.com>
2023-07-05 17:06:04 +0545

    leave

--- src/Components/Pages/Dashboard/Dashboard.jsx
+++ src/Components/Pages/Dashboard/Dashboard.jsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import UserAuthContextApi, {
   UserAuthContext,
 } from "../../../Hoc/ContextApi/UserAuthContextApi";
+import { get } from "../../../services/api";
+import LeaveChart from "./LeaveChart";
 
 const Dashboard = () => {
+  const [singleEmployee, setSingleEmployee] = useState([]);
   const storedUserId = localStorage.getItem("emp_id");
-  console.log(storedUserId);
+
+  useEffect(() => {
+    console.log("hello");
+    get(`/employee/${storedUserId}`)
+      .then((res) => {
+        console.log(res.data);
+        setSingleEmployee(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
   const navigate = useNavigate();
 
@@ -14,13 +28,25 @@ const Dashboard = () => {
     window.localStorage.clear();
     navigate("/login");
   };
-
   return (
     <UserAuthContextApi>
       <UserAuthContext.Provider>
         <div>profile</div>
         <div>
+          {singleEmployee.map((val, i) => {
+            return (
+              <div key={i}>
+                <div>{val.salary}</div>
+                <p>{val.emp_id}</p>
+              </div>
+            );
+          })}
+        </div>
+        <div>
           <button onClick={removeToken}>logout</button>
+        </div>
+        <div>
+          <LeaveChart />
         </div>
       </UserAuthContext.Provider>
     </UserAuthContextApi>
