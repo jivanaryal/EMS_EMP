ea40ae40ad663af868a79d925dff8a7242b83cd9
jivan aryal <jeevanaryal7947@gmail.com>
2023-07-09 22:16:05 +0545

    hello

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import Dashboard from "./Components/Pages/Dashboard/Dashboard";
 import LoginPage from "./Components/Pages/LoginPage/LoginPage";
 import "react-toastify/dist/ReactToastify.css";
 import Leave from "./Components/Pages/LeaveRequest/Leave";
+import Task from "./Components/Pages/Task/Task";
+import NewTask from "./Components/Pages/Task/NewTask";
+import InProgressTask from "./Components/Pages/Task/InProgressTask";
+import CompletedTask from "./Components/Pages/Task/CompletedTask";
+import MyProfile from "./Components/Navigation/Navbar/Profile/MyProfile";
 
 const App = () => {
   return (
@@ -14,6 +19,11 @@ const App = () => {
           <Route path="" element={<Layout />}>
             <Route index element={<Dashboard />} />
             <Route path="leave" element={<Leave />} />
+            <Route path="task" element={<Task />} />
+            <Route path="task/new" element={<NewTask />} />
+            <Route path="task/inprogress" element={<InProgressTask />} />
+            <Route path="task/completed" element={<CompletedTask />} />
+            <Route path="/myprofile" element={<MyProfile />} />
           </Route>
           <Route path="/login" element={<LoginPage />} />
         </Routes>
