c5ace176b4fe6c18605730abf1a4a0d280a24586
jivan aryal <jeevanaryal7947@gmail.com>
2023-07-13 16:02:25 +0545

    hello

--- src/Components/UI/DangerModal.jsx
+++ src/Components/UI/DangerModal.jsx
@@ -107,6 +107,20 @@ const Index = ({ name, onClick, falseCondition }) => {
                     </Field>
                   </div>
                 </div>
+                <div className="flex justify-center mt-9 gap-6">
+                  <div
+                    className="px-4 py-2 bg-gray-500 text-white font-semibold rounded-md transition-colors duration-300 mb-3"
+                    onClick={fail}
+                  >
+                    Cancel
+                  </div>
+                  <button
+                    type="submit"
+                    className="px-4 py-2 bg-blue-500 text-white font-semibold rounded-md hover:bg-blue-600 transition-colors duration-300 mb-3"
+                  >
+                    Update
+                  </button>
+                </div>
               </Form>
             )}
           </Formik>
