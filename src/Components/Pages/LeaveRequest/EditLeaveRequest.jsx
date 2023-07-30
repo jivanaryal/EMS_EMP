7a87cf547dbed9c9149bee21acbd8de43a6dce85
manojbelbase <manojbelbase56@gmail.com>
2023-07-31 00:22:59 +0545

    sd

--- src/Components/Pages/LeaveRequest/EditLeaveRequest.jsx
+++ src/Components/Pages/LeaveRequest/EditLeaveRequest.jsx
@@ -101,7 +101,7 @@ const LeaveRequest = () => {
                 type="submit"
                 className="px-4 py-2 bg-blue-500 text-white font-semibold rounded-md hover:bg-blue-600 transition-colors duration-300"
               >
-                Add Leave
+                Update Leave
               </button>
             </div>
           </Form>
--- src/Components/Pages/LoginPage/ChangePassword.jsx
+++ src/Components/Pages/LoginPage/ChangePassword.jsx
@@ -46,17 +46,17 @@ const ChangePassword = () => {
       const response = await post(`/createemp/changepw/${id}`, values);
 
       if (response.status === 200) {
-        toast.success("New employee created");
+        toast.success("New password change");
       } else {
-        console.error("Failed to create employee.");
-        toast.error("Failed to create employee.");
+        // console.error("Failed to create employee.");
+        toast.error("Failed to change password.");
       }
     } catch (error) {
       if (error.response && error.response.status === 400) {
-        console.error("Employee with the same ID already exists.");
-        toast.error(".Your Old Password is incorrect.");
+        // console.error("Employee with the same ID already exists.");
+        toast.error("Your Old Password is incorrect.");
       } else {
-        console.error("Error:", error);
+        // console.error("Error:", error);
         toast.error("An error occurred while creating employee.");
       }
     }
